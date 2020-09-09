cd8dc508e0469f623f55540ce3207460e67a2d7c
unknown <jwade1327@gmail.com>
2020-09-09 09:51:48 -0500

    models-index

--- models/Product.js
+++ models/Product.js
@@ -21,27 +21,27 @@ Product.init(
       allowNull: false
     },
     price: {
-      type: DataTypes.DECIMAL,
+      type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
       validate: {
         isDecimal: true
-      }
+      },
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      defaultValue: '10',
+      defaultValue: 10,
       validate: {
         isNumeric: true
-      }
+      },
     },
     catedgory_id: {
       type: DataTypes.INTEGER,
       references: {
         model: 'category',
         key: 'id'
-      }
-    }
+      },
+    },
   },
   {
     sequelize,
